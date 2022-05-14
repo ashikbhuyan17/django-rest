a7b5bde21f04e8e18163e32a71d0b8a12620514b
ashik-bhuyan17 <ashik.ajke@gmail.com>
2022-05-14 23:20:10 +0600

    mm

--- Filtering_and_django_filter(22)/djangofilter/filter/views.py
+++ Filtering_and_django_filter(22)/djangofilter/filter/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
-from rest_framework.response import Response
 
 # Create your views here.
 
